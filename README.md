d1ffdbcff8ea0b70e92868d77121688972261e68
oregone1 <henryjames2007@gmail.com>
2020-12-09 00:44:05 -0800

    updated readme.md for accuracy updated config.json with better configuation fixed all three issues noted in previous commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # music-script
 a thing that just trees ~/Music and acts as a cli ipod
+
+you can edit config.json to change the directory that it looks for music in
--- main.py
+++ main.py
@@ -6,12 +6,13 @@ import os
 with open('config.json', 'r') as f:
     data = json.load(f)
 
+update = True
 run = True
-music_dir = data['config']['music_dir']
+music_dir = data['config']['music-dir']
 file_list = []
 _input = {}
 user_input = ''
-extension_list = ['.mp3', '.webm']
+extension_list = data['config']['recognized-files']
 
 os.chdir('/')
 
@@ -27,29 +28,39 @@ def list_files(file_list):
     for i, file in enumerate(file_list):
         print(f'{i}, {file}')
         _input[i] = file
-    return(_input)
     print('-----------------------------')
-
-#
-
-print("type help if you get stuck")
-
-#get_dir_contents(music_dir)
-#ui(file_list)
-#print(_input)
-
-#//: run part of the script
+    return(_input)
 
 while run:
-    get_dir_contents(music_dir)
-    list_files(file_list)
-    #change_dir(user_input)
-    user_input = input(data['config']['prompt-name'])
-    if len(user_input) > 0:
-        music_dir += '/' + _input[int(user_input)]
-        for extension in extension_list:
-            if extension in music_dir:
-                os.system(f'cvlc {music_dir}')
-                music_dir = music_dir - _input[int(user_input)]
-    else:
-        print('error')
+    try:
+        if update:
+            music_dir2 = music_dir
+            get_dir_contents(music_dir)
+            list_files(file_list)
+            user_input = input(data['config']['prompt-name'])
+            if len(user_input) > 0:
+                music_dir += '/' + _input[int(user_input)]
+                for extension in extension_list:
+                    if extension in music_dir:
+                        update = False
+                    else:
+                        pass # TODO: log
+            else:
+                #print('error') # TODO: log
+                split_dir = music_dir.split('/')
+                del split_dir[-1]
+                #    print(split_dir)
+                music_dir = '/'.join(split_dir)
+                #    print(music_dir) # TODO: clean this up
+        else:
+            try:
+                os.system(f'ffplay \'{music_dir}\' -autoexit -nodisp')
+                music_dir = music_dir2
+                update = True
+            except:
+                print('error: file could not be played')
+                music_dir = music_dir2
+                update = True
+
+    except:
+        pass # TODO: log error
