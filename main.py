5955206c2d2d4bea2597df22b1106e8c18b364ff
oregone1 <henryjames2007@gmail.com>
2020-12-23 15:42:15 -0800

    updated user input backend to work better and added 'download' function that searches youtube for a file to download.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+from youtube_search import YoutubeSearch
 import json
 import os
+import youtube_dl
 
 with open('config.json', 'r') as f:
     data = json.load(f)
@@ -28,7 +30,31 @@ def list_files(file_list):
     return(_input)
 
 def play(music_dir):
-    os.system(f'mpv --no-video --speed={int(data["config"]["back-speed"])} \'{music_dir}\'')
+    os.system(f'mpv --no-video --speed={float(data["config"]["back-speed"])} \'{music_dir}\'')
+
+def download_file(title):
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'webm',
+            'preferredquality': '192',
+            }]
+        }
+
+    is_video = 'n'
+    i = -1
+    results = YoutubeSearch(title, max_results=10).to_dict()
+    while is_video.lower() == 'n':
+        i += 1
+        print(f'found {results[i]["title"]}')
+        is_video = input('is the title correct? (y/n) ')
+
+    download = 'https://www.youtube.com' + str(results[i]['url_suffix'])
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print("Downloading audio now\n")
+        ydl.download([download])
 
 while run:
     try:
@@ -53,19 +79,19 @@ while run:
         try:
             if user_input == 'exit':
                 run = False
-            elif 'play' in user_input:
+            elif user_input.split(' ')[0] == 'play':
                 play(music_dir + '/' + _input[int(user_input.replace('play', ''))])
-            elif 'list' in user_input:
+            elif user_input.split(' ')[0] == 'list':
                 index = user_input.replace('list', '').split(',')
                 print(index)
                 for num in index:
                     play(music_dir + '/' + _input[int(num)])
 
-            elif 'config' in user_input:
+            elif user_input.split(' ')[0] == 'config':
                 if len(user_input) == 6:
                     for option in data['config']:
                         print(' ',option)
-                elif 'edit' in user_input:
+                elif user_input.split(' ')[1] == 'edit':
                     property = user_input.replace(f'config edit ', '')
                     property = property.split(' ')
                     print(data)
@@ -75,11 +101,15 @@ while run:
                     #data["config"]["playback-speed"] = "2"
                     with open(real_dir, 'w') as w:
                         json.dump(data, w, indent=2)
+
+            elif user_input.split(' ')[0] == 'download':
+                download_file(user_input.replace('download', ''))
+
             else:
                 print('error') # detailed error handling
                 break
 
         except Exception as e:
             print('error', e)
-            
+
 print('exiting')
