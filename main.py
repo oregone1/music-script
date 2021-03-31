785c8eba4a437a677b866d3d2957eb224b687dc9
oregone1 <henryjames2007@gmail.com>
2021-03-30 17:44:15 -0700

    rewrote from the ground up, cleaned things up a bit, and added proper type declarations to everything. removed some unnesecery features.

--- main.py
+++ main.py
@@ -1,127 +1,118 @@
 #!/usr/bin/env python3
 
 from youtube_search import YoutubeSearch
+from time import sleep
+from typing import Union
 import json
 import os
-import youtube_dl
 
-with open('config.json', 'r') as f:
-    data = json.load(f)
+# for error handling in handle_ui()
+class ConnectionError(Exception):
+    pass
 
-run = True
-music_dir = data['config']['music-dir']
-file_list = []
-_input = {}
-user_input = ''
-extension_list = data['config']['recognized-files']
-real_dir = os.path.realpath('config.json')
+# not at all necessary i just felt like it
+def IO () -> None: # haskell keyword go brrrr
+    return None
 
-def get_dir_contents(directory):
-    file_list.clear()
-    for file in os.listdir(directory):
-        file_list.append(file)
+run: bool = True
+CONFIG_LOCATION: str = os.getcwd() + '/config.json'
 
-def list_files(file_list):
-    _input.clear()
-    for i, file in enumerate(file_list):
-        print(f'{i}, {file}')
-        _input[i] = file
-    print('-----------------------------')
-    return(_input)
+with open(CONFIG_LOCATION, 'r') as f:
+    json_data: dict = json.load(f)
+    current_dir: str = json_data["config"]["music-dir"]
 
-def play(music_dir):
-    os.system(f'mpv --no-video --speed={float(data["config"]["back-speed"])} \'{music_dir}\'')
+print("loaded config at: " + CONFIG_LOCATION)
 
-def download_file(title):
+# order files for get_input()
+def get_dir_contents(directory: str) -> dict:
+    files: list = [file for file in os.listdir(directory)]
+    return dict(enumerate(files))
 
-    is_video = 'n'
-    i = -1
+def get_input(file_addrs: dict) -> str: # parameter should be called different thing but i am lazy
+    print("-"*15)
+    [print(f"{file[0]}: {file[1]}") for file in file_addrs.items()]
+    print("-"*15)
+    return input("-> ")
 
-    print(f'\\\\\\ {title}')
+def handle_ui(i: str, select_from: dict) -> Union[str, bool, IO ()]: # TODO: when python3.10 is out adapt to new switch statement
+    try: # if select_from can be indexed by i
+        os.system("clear")
+        return select_from[int(i)]
+    except: # this is probably bad, yes
+        try: # particularly this part
+            command: list = i.split(' ')
+            if command[0] == "play":
+                play(command[1].split(','), select_from)
+            elif command[0] == "download":
+                download(command[1:])
+            elif command[0] == "file":                                # set type shouldnt be here but it is
+                os.mkdir(command[1] if command[-1] == command[1] else str(command[1] + '-' + '-'.join(command[2:])))
+            elif command[0] == "del":
+                os.system(f"rm -rf \'" + select_from[int(command[1])] + "\'") # not using os.remove as it cant delete directories
+            elif command[0] == "edit":
+                edit_json(' '.join(command[1:]))
+            elif command[0] == "exit":
+                return False
+            elif command[0] == '':
+                return 'back'
+            else:
+                print("unknown command")
 
-    if 'https://www.youtube.com' in title:
-        download = title
+        except ValueError:
+            os.system("clear")
+            print('something is wrong, maybe you input a non integer?')
+            sleep(1)
+        except ConnectionError:
+            os.system("clear")
+            print("something is wrong, maybe internet?")
+            sleep(1)
 
-    else:
-        results = YoutubeSearch(title, max_results=10).to_dict()
-        #print(results)
-        while is_video.lower() == 'n':
-            i += 1
-            print(f'found {results[i]["title"]}')
-            is_video = input('is the title correct? (y/n) ')
+def play(files: list, file_dict: dict) -> IO ():
+    for file in files:
+        os.system("clear")
+        os.system(f'mpv --no-video --speed={float(json_data["config"]["back-speed"])} \'{file_dict[int(file)]}\'')
 
-        download = 'https://www.youtube.com' + str(results[i]['url_suffix'])
+def download(search: str) -> IO ():
+    search_dict: dict = {"results":[{"title": "clear me"}]}
 
     try:
-        print('downloading ' + download)
-        os.system('youtube-dl -f 251 ' + str(download))
+        results: dict = YoutubeSearch(' '.join(search), max_results=10).to_dict()
+        search_dict.clear()
+        print(search_dict)
+        for i in range(len(results)):
+            search_dict[i]: str = results[i]["title"].replace('\n', '')
     except:
-        print('make sure you have youtube-dl installed for this to work')
+        raise ConnectionError
+
+    select: int = int(get_input(search_dict))
+
+    print('downloading ' + results[select]["title"])
+    os.system("youtube-dl -f 251 https://www.youtube.com" + results[select]["url_suffix"])
+
+def edit_json(user_input: str='') -> IO ():
+    print(json_data)
+    json_data['config'][user_input.split(':')[0]] = user_input.split(':')[1]
+    with open(CONFIG_LOCATION, 'w') as f:
+        json.dump(json_data, f, indent=2)
+    os.system("clear")
+    print(json_data)
 
 while run:
     try:
-        os.chdir(music_dir)
-        print('-----------------------------')
-        get_dir_contents(music_dir)
-        list_files(file_list)
-        user_input = input(data['config']['prompt-name'])
-        os.system('clear')
-        music_dir2 = music_dir
-        if len(user_input) > 0:
-            music_dir += '/' + _input[int(user_input)]
+        os.chdir(current_dir)
+        file_addrs: dict = get_dir_contents(current_dir)
+        ui: str = get_input(file_addrs) # ui as in user input not user interface
+        handle_result: Union[str, bool, None] = handle_ui(ui, file_addrs)
+        if handle_result == 'back':
+            current_dir = '/'.join(current_dir.split('/')[:-1])
+        elif type(handle_result) == str:
+            current_dir = current_dir + '/' + handle_result
+        elif type(handle_result) == bool:
+            run = handle_result
 
-        else:
-            split_dir = music_dir.split('/')
-            del split_dir[-1]
-            #debug    print(split_dir)
-            music_dir = '/'.join(split_dir)
-            #debug    print(music_dir)
+    except NotADirectoryError: # take care of dumb user that tries to cd into file
+        play(ui, file_addrs)
+        current_dir: str = '/'.join(current_dir.split('/')[:-1])
 
-    except:
-        try:
-            command = user_input.split(' ')
-
-            if command[0] == 'exit':
-                run = False
-
-            if command[0] == 'help':
-                pass ## wip
-
-            elif command[0] == 'play':
-                play(music_dir + '/' + _input[int(user_input.replace('play', ''))])
-
-            elif command[0] == 'list':
-                index = user_input.replace('list', '').split(',')
-                print(index)
-                for num in index:
-                    play(music_dir + '/' + _input[int(num)])
-
-            elif command[0] == 'config':
-                if len(user_input) == 6:
-                    for option in data['config']:
-                        print(' ',option)
-                elif command[1] == 'edit':
-                    property = user_input.replace(f'config edit ', '')
-                    property = property.split(' ')
-                    print(data)
-                    #if not str(property[0]) == 'config' and str(property[1]) == 'edit':
-                    print('test')
-                    data["config"][property[0]] = str(property[1])
-                    #data["config"]["playback-speed"] = "2"
-                    with open(real_dir, 'w') as w:
-                        json.dump(data, w, indent=2)
-
-            elif command[0] == 'download':
-                download_file(user_input.replace('download', ''))
-
-            elif command[0] == 'file':
-                os.mkdir(command[1])
-
-            else:
-                print('error') # detailed error handling
-                break
-
-        except Exception as e:
-            print('error', e)
-
-print('exiting')
+    except Exception as e:
+        print("uh oh something bad happened: " + str(e))
